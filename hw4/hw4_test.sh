c5e11b9505e2d2952d3b0c45926b39d7ff69eb63
b01901143 <b01901143@ntu.edu.tw>
2017-12-08 21:20:02 +0800

    final version

--- hw4/hw4_test.sh
+++ hw4/hw4_test.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+wget 'https://www.dropbox.com/s/hb3fpwb8rv989rj/embedding?dl=0' -O 'embedding'
+wget 'https://www.dropbox.com/s/zagcquxhrt00fqf/embedding.syn1neg.npy?dl=0' -O 'embedding.syn1neg.npy' 
+wget 'https://www.dropbox.com/s/dp43h7t3tvzq31p/embedding.wv.syn0.npy?dl=0' -O 'embedding.wv.syn0.npy'
 python3 test.py $1 $2 check_point/pretrain-100-0.83.hdf5 
 
